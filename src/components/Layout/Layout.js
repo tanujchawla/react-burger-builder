fdf8ab0c49f6024aef24afbba5b19a6219fd4fc3
Tanuj Chawla <tanuj.chawla@biz2credit.org>
2020-02-17 20:00:33 +0530

    till video 176

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Aux from '../../hoc/Auxillary';
 import classes from './Layout.module.css';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
