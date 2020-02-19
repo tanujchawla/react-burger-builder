3162b0351501f6fb89e69f798737708c439d8016
Tanuj Chawla <tanuj.chawla@biz2credit.org>
2020-02-19 13:14:51 +0530

    till video 188

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Auxillary';
+import Aux from '../../../hoc/Auxillary/Auxillary';
 
 import classes from './SideDrawer.module.css';
 
